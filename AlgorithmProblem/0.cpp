45d189da6f8f1835c230d0a2ccdbdbfc70d8480a
DelightLane <hano881112@hanmir.com>
2017-03-14 01:25:11 +0900

    Daily Algorithm 프로그래밍 시작

---
+++ AlgorithmProblem/0.cpp
@@ -0,0 +1,38 @@
+#include "0.h"
+#include <cassert>
+
+int Comparision::max(int value1, int value2)
+{
+	if (value1 > value2)
+	{
+		return value1;
+	}
+	else if (value1 < value2)
+	{
+		return value2;
+	}
+
+	assert(0);
+
+	return 0;
+}
+
+int Comparision::min(int value1, int value2)
+{
+	return this->max(value1, value2) == value1 ? value2 : value1;
+}
+
+int Comparision::maximum(int* arr, int size)
+{
+	int max = -1;
+
+	for (int i = 0; i < size; ++i)
+	{
+		if (max < arr[i])
+		{
+			max = arr[i];
+		}
+	}
+
+	return max;
+}
---
+++ AlgorithmProblem/0.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class Comparision
+{
+public:
+	int max(int value1, int value2);
+	int min(int value1, int value2);
+	int maximum(int* arr, int size);
+};
